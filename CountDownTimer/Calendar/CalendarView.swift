babee89c08071f1b84152ff653aa533b34aeb6d2
ns19146 <ns19146@tomakomai.kosen-ac.jp>
2023-01-03 14:40:45 +0900

    各画面の仮置き

---
+++ CountDownTimer/Calendar/CalendarView.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarView.swift
+//  CountDownTimer
+//
+//  Created by 二瓶千寿 on 2023/01/03.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+    }
+}
---
+++ CountDownTimer/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  CountDownTimer
+//
+//  Created by 二瓶千寿 on 2023/01/03.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
