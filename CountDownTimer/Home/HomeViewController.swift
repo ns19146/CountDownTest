0b26814ea1739cfd36d83227ca2af8778a81f3c3
ns19146 <ns19146@tomakomai.kosen-ac.jp>
2023-01-03 15:48:35 +0900

    UIKitとSwiftUIの連携

--- CountDownTimer/Home/HomeViewController.swift
+++ CountDownTimer/Home/HomeViewController.swift
@@ -6,13 +6,23 @@
 //
 
 import UIKit
+import SwiftUI
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //SwiftUIとUIKitの接続
+        let hostingController = UIHostingController(rootView: HomeView())
+        addChild(hostingController)
+        view.addSubview(hostingController.view)
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                                     hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+                                     hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                                     hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
+        hostingController.didMove(toParent: self)
     }
     
 
